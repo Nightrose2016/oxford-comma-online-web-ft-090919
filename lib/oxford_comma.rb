27a864d1c619db2119a48330acb4be6508337694
Lialah Barrer <lrbarrer@gmail.com>
2019-09-14 17:55:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return "#{array[0]} and #{array[1]}"
   elsif array.length == 3
-   return array.join("," "and")
+   return array.join(" , " " and ")
   else
     return array.join(",")
   end
